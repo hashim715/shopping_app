e0db865e58527e29ee06d71d74bff183bcd66331
hashim715 <mharus575@gmail.com>
2022-11-30 19:45:02 +0500

    first commit

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import mimetypes
-mimetypes.add_type("text/html", ".css", True)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
